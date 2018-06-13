e37769bec7cd830908be9241eaaa8938dc980a3e
Andrey Falko <afalko@salesforce.com>
2018-06-12 18:42:10 -0700

    Upgrade to revised interface

--- src/main/java/net/afalko/zipkin/filter/GenericEnrichmentFilter.java
+++ src/main/java/net/afalko/zipkin/filter/GenericEnrichmentFilter.java
@@ -1,51 +1,29 @@
 package net.afalko.zipkin.filter;
 
-import zipkin.Annotation;
-import zipkin.Span;
-import zipkin.filter.FilterActivatedException;
-import zipkin.filter.SpanFilter;
-import zipkin.storage.Callback;
+import zipkin2.Callback;
+import zipkin2.Span;
+import zipkin2.collector.CollectorMetrics;
+import zipkin2.collector.filter.SpanFilter;
 
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class GenericEnrichmentFilter implements SpanFilter {
 
-    public static final String ZIPKIN_V1_ANNOTATION = "ZIPKIN_V1_ANNOTATION";
-    public static final String ZIPKIN_V2_TAG = "ZIPKIN_V2_TAG";
+    public static final String ZIPKIN_TAG = "ZIPKIN_TAG";
 
-    public List<Span> processV1(List<Span> spans, Callback<Void> callback) {
-        if (System.getenv(ZIPKIN_V1_ANNOTATION) == null) {
-            return spans;
-        }
-        String v1Tag = System.getenv(ZIPKIN_V1_ANNOTATION);
-
-        return spans.stream()
-                .map(span -> span.toBuilder()
-                        .addAnnotation(Annotation.builder()
-                                .timestamp(Instant.now().truncatedTo(ChronoUnit.MICROS).toEpochMilli())
-                                .value(v1Tag)
-                                .build())
-                        .build())
-                .collect(Collectors.toList());
-    }
-
-    public List<zipkin2.Span> processV2(List<zipkin2.Span> spans, Callback<Void> callback) {
-        if (System.getenv(ZIPKIN_V2_TAG) == null) {
+    public List<Span> process(List<Span> spans, CollectorMetrics metrics, Callback<Void> callback) {
+        if (System.getenv(ZIPKIN_TAG) == null) {
             return spans;
         }
         // TODO: Error handling
-        String[] v2Tag = System.getenv(ZIPKIN_V2_TAG).split(":");
-        String v2Key = v2Tag[0];
-        String v2Value = v2Tag[1];
+        String[] tag = System.getenv(ZIPKIN_TAG).split(":");
+        String key = tag[0];
+        String value = tag[1];
 
         return spans.stream()
                 .map(span -> span.toBuilder()
-                        .putTag(v2Key, v2Value)
+                        .putTag(key, value)
                         .build())
                 .collect(Collectors.toList());
     }
--- src/main/java/net/afalko/zipkin/filter/GenericEnrichmentFilterAutoConfiguration.java
+++ src/main/java/net/afalko/zipkin/filter/GenericEnrichmentFilterAutoConfiguration.java
@@ -2,7 +2,7 @@ package net.afalko.zipkin.filter;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import zipkin.filter.SpanFilter;
+import zipkin2.collector.filter.SpanFilter;
 
 @Configuration
 public class GenericEnrichmentFilterAutoConfiguration {
